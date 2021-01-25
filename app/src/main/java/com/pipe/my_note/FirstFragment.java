44ea0d5ce69be61054ce263e0db81bd5d065348d
galaxy1 <st989@ya.ru>
2021-01-25 22:46:54 +0400

    Получилось запустить первый фрагмент со списком

--- app/src/main/java/com/pipe/my_note/FirstFragment.java
+++ app/src/main/java/com/pipe/my_note/FirstFragment.java
@@ -1,34 +1,131 @@
 package com.pipe.my_note;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class FirstFragment extends Fragment {
 
+    private boolean isLandscape;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    public static FirstFragment newInstance(String param1, String param2) {
+        FirstFragment fragment = new FirstFragment();
+        Bundle args = new Bundle();
+        /*args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        */
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Определение, можно ли будет расположить рядом герб в другом фрагменте
+        isLandscape = getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE;
+
+        // Если можно нарисовать рядом герб, то сделаем это
+        if (isLandscape) {
+            showLandTheCard(0);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+    /*        mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+    */
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+        initList(view);
     }
+
+    private void initList(View view) {
+        LinearLayout layoutView = (LinearLayout) view;
+        String[] cities = getResources().getStringArray(R.array.tags);
+
+        // В этом цикле создаём элемент TextView,
+        // заполняем его значениями,
+        // и добавляем на экран.
+        // Кроме того, создаём обработку касания на элемент
+        Context context = getContext();
+        for (int i = 0; i < cities.length; i++) {
+            if (context != null) {
+                String city = cities[i];
+                TextView textView = new TextView(context);
+                textView.setText(city);
+                textView.setTextSize(30);
+                layoutView.addView(textView);
+                final int fi = i;
+                textView.setOnClickListener(v -> showTheCard(fi));
+            }
+        }
+    }
+
+    private void showTheCard(int index) {
+        if (isLandscape) {
+            showLandTheCard(index);
+        } else {
+            showPortTheCard(index);
+        }
+    }
+
+    private void showLandTheCard(int index) {
+        // Создаём новый фрагмент с текущей позицией для вывода герба
+        SecondFragment detail = SecondFragment.newInstance(index);
+
+        // Выполняем транзакцию по замене фрагмента
+        FragmentActivity context = getActivity();
+        if (context != null) {
+            FragmentManager fragmentManager = context.getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.zettelkasten, detail);  // замена фрагмента
+            //fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.commit();
+        }
+    }
+
+    private void showPortTheCard(int index) {
+        // Откроем вторую activity
+        Context context = getContext();
+        if (context != null) {
+            Intent intent = new Intent(context, SecondActivity.class);
+            // и передадим туда параметры
+            intent.putExtra(SecondFragment.ARG_INDEX, index);
+            startActivity(intent);
+        }
+    }
+
 }
--- app/src/main/java/com/pipe/my_note/SecondFragment.java
+++ app/src/main/java/com/pipe/my_note/SecondFragment.java
@@ -1,34 +1,54 @@
 package com.pipe.my_note;
 
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 public class SecondFragment extends Fragment {
 
+    static final String ARG_INDEX = "index";
+    private int index;
+
+    public static SecondFragment newInstance(int index) {
+        SecondFragment f = new SecondFragment();    // создание
+        // Передача параметра
+        Bundle args = new Bundle();
+        args.putInt(ARG_INDEX, index);
+        f.setArguments(args);
+        return f;
+    }
+
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_INDEX);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
+        AppCompatImageView imageCoatOfArms = view.findViewById(R.id.zettelkasten);
+        // Получить из ресурсов массив указателей на изображения гербов
+        TypedArray images = getResources().obtainTypedArray(R.array.text);
+        // Выбрать по индексу подходящий
+        imageCoatOfArms.setImageResource(images.getResourceId(index, -1));
     }
 }
